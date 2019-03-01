b92d4aa44111571729e0495ac5a8be8d02c537ab
lhj <3282657310@qq.com>
2019-03-01 22:44:01 +0800

    111

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app=express();
 const path=require('path');
 const Youch=require('youch');
 const artTemplate=require('express-art-template');
-const createError=require('http-errors');
+// const createError=require('http-errors');
 const favicon=require('express-favicon');
-
 // 中间件处理函数
 //处理静态资源
-app.use('/',express.static(path.join(__dirname,'public')));
+app.use('/',express.static(path.join(__dirname,'./public')));
+const middlewares=require('./middlewares.js');
+//自定义中间件
+app.use(middlewares.global);
+const routers=require('./routers');
 //配置模板引擎art-template
 app.engine('art',artTemplate);
 
@@ -17,17 +20,9 @@ app.set('view options', {debug: process.env.NODE_ENV === 'development'});
 //小图标统一特殊处理,也是express的方法
 app.use(favicon(path.join(__dirname,'favicon.ico')));
 // 定义业务路由,写一个服务器错误
-app.get('/',(req,res,next)=>{
-    //写一个错误代码然后传入到错误处理程序中,一共有三种写法第一种是
-    // const error=new Error();
-    // error.status=500;
-    // next(error);
-    // 第二种是throw new Error('自己抛出的异常').status=500;
-    // 第三种是throw createError(500,'serverError');使用第三种写法
-    throw createError(500,'serverError')
-    // res.send('<p>some html</p>');
-});
-//写一个浏览器找不到的404错误,如果走到这个中间件,证明所以url都不符合条件
+app.use(routers);
+
+//写一个浏览器找不到的404错误,如果走到这个中间件,证明所有url都不符合条件
 app.use((req,res,next)=>{
     const error = new Error('Not Found')
   error.status = 404
@@ -47,7 +42,6 @@ app.use((err,req,res,next)=>{
         res.render('error.art',{status:err.status})
     }
 });
-
 
 app.listen(3000,()=>{
     console.log('正在监听端口3000');
